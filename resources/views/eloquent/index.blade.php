9772d274d2cfa19c833c80e6c025bccbb979fbb0
Aleff <aleffbruno@gmail.com>
2017-11-13 12:17:36 -0300

    delete USER implementado

--- resources/views/eloquent/index.blade.php
+++ resources/views/eloquent/index.blade.php
@@ -1,4 +1,5 @@
 <!--  <a href={{ route('eloquent.account.create') }}> Criar nova Account</a></br></br>-->
+<a href={{ route('eloquent.user.createuser') }}> Criar nova Account</a></br></br>
 Listagem de usuarios
 
 <table border="1">
@@ -16,7 +17,7 @@ Listagem de usuarios
 				<td>{{$user->id}}</td>
 				<td>{{$user->email}}</td>
 				<td>{{$user->name}}</td>
-				<td><a href="{{route('eloquent/users')}}">deletar</a></td>
+				<td><a href="{{route('eloquent.user.delete',$user->id)}}">deletar</a></td>
 			</tr>
 		@endforeach
 	</tbody>
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::group(['prefix'=>'eloquent','as'=>'eloquent.'],function(){
 	Route::get('accounts','EloquentAccountsController@index')->name('account.list');
 	Route::get('accounts/create','EloquentAccountsController@create')->name('account.create');
 	Route::post('accounts/store','EloquentAccountsController@store')->name('account.store');
-	Route::get('users/createuser','EloquentUsersController@createuser')->name('users.createuser');
+	Route::get('users/createuser','EloquentUsersController@createuser')->name('user.createuser');
 	Route::post('users/storeuser','EloquentUsersController@storeuser')->name('user.store');
 	Route::get('users','EloquentUsersController@index')->name('user.list');
 	Route::get('users/delete/{id}','EloquentUsersController@deleteuser')->name('user.delete');
